a9eb3d4bb1470ecea32e782d5a7693c58c091358
MD IRFAN <Irfanmd2503@gmail.com>
2021-07-13 20:28:31 +0530

    52 ms

--- solutions/23. Merge k Sorted Lists.cpp
+++ solutions/23. Merge k Sorted Lists.cpp
@@ -10,32 +10,30 @@
  */
 class Solution {
 public:
-   struct comp
-     {
-        bool operator()(const ListNode *lhs, const ListNode *rhs) const {
-            return lhs->val > rhs->val;
-        }
-    };
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode *head = NULL;
-        int listSize = lists.size();
-        if(listSize==0) return head;
+        ListNode *head=NULL;
+        int listSize=lists.size();
+        if(listSize == 0) return head;
         
-        priority_queue<ListNode*, vector<ListNode*>,comp >pq;
-        for(int i=0;i<listSize;i++){
-            if(lists[i]!=NULL) pq.push(lists[i]);
-        }
-        
-        ListNode *temp=NULL,*last=NULL;
-        while(!pq.empty()){
-            temp = pq.top();
-            pq.pop();
-            if(head==NULL){
-                head = last = temp;
-            }else{
-                last->next = temp;
-                last = last->next;
-            }
-            if(temp->next!=NULL) pq.push(temp->next);
-        }
-        return head;
+        int low =0,high = listSize-1;
+        ListNode *curr = DivideAndMerge(lists,low,high);
+        return curr;
+    }
+    
+   ListNode *DivideAndMerge(vector<ListNode*>& lists,int low,int high){
+       while(low<high){
+           lists[low] = mergeList(lists[low],lists[high]); 
+           low++;
+           high--;
+           if(low>=high){
+               low=0;
+               high =high;
+           }
+       }
+        // if(low<high){
+        //     int mid = low + (high -low)/2;
+        //     ListNode *l1= DivideAndMerge(lists,low,mid);
+        //     ListNode *l2 =DivideAndMerge(lists,mid+1,high);
+        //     mergeList(l1,l2);
+        // }
+        return lists[0]; // return something
